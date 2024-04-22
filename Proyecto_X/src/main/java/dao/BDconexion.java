ffc377e87e48bb9183b8c6059b1566c2d5bbfa99
mbgco <mbgco@PcMBGcode>
2024-04-22 20:16:15 +0200

    Agregamos conexion BD

---
+++ Proyecto_X/src/main/java/dao/BDconexion.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class BDconexion {
+
+	public static final String JDBC_URL = "jdbc:mysql://localhost:3306/flota";
+	public static Connection instance = null;
+	
+	
+	public static Connection getmiconexion() throws SQLException {
+		
+		if (instance ==null) {
+			Properties props = new Properties ();
+			props.put("user", "root");
+			props.put("password", "");
+			props.put("charset", "UFT-8");
+			
+			instance = DriverManager.getConnection(JDBC_URL,props);
+		}
+		
+		return instance;
+	}
+	
+}
